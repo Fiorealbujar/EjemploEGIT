d5c90fa143c00b00612ce43f3e34e4679eb41ec6
Fiorealbujar <fiorerut8@hotmail.com>
2026-02-02 09:43:09 +0100

    Segundo commit

--- src/EjemploEGIT/EjemploEGIT.java
+++ src/EjemploEGIT/EjemploEGIT.java
@@ -4,7 +4,7 @@ public class EjemploEGIT {
 
 	public static void main(String[] args) {
 		System.out.println("Ejemplo GIT desde Eclipse");
-
+		System.out.println("Primer cambio desde master");
 	}
 
 }
