5eedd22fc172a5c5c09f905614ee7b8e1d3062e9
Fiorealbujar <fiorerut8@hotmail.com>
2026-02-09 09:11:19 +0100

    Segundo Commit Rama 1

--- src/EjemploEGIT/EjemploEGIT.java
+++ src/EjemploEGIT/EjemploEGIT.java
@@ -5,7 +5,8 @@ public class EjemploEGIT {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo GIT desde Eclipse");
 		System.out.println("Primer cambio desde master");
-		System.out.println("Primer cambio rama 1");
+		System.out.println("Primer cambio Rama 1");
+		System.out.println("Segundo cambio Rama 1");
 	}
 
 }
