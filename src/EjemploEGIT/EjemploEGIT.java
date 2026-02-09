9cae20c00c9b929ff867f0f00740aa5432637318
Fiorealbujar <fiorerut8@hotmail.com>
2026-02-09 09:03:27 +0100

    Primer commit rama1

--- src/EjemploEGIT/EjemploEGIT.java
+++ src/EjemploEGIT/EjemploEGIT.java
@@ -5,6 +5,7 @@ public class EjemploEGIT {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo GIT desde Eclipse");
 		System.out.println("Primer cambio desde master");
+		System.out.println("Primer cambio rama 1");
 	}
 
 }
